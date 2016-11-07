3fb5bc60231f99cc62cc67298e69259492ff4958
Jorge Rodriguez <jorge.rodriguez@ariadna.com.ec>
2016-11-07 16:38:03 -0500

    Sportage R CKD en consulta

--- protected/views/gestionConsulta/_form.php
+++ protected/views/gestionConsulta/_form.php
@@ -1045,7 +1045,7 @@ if ($cargo_id != 46 && $cargo_adicional != 0 && $cargo_id != 70) {
                                         "21" => "Sportage Active",
                                         "95" => "Sportage GT",
                                         "83" => "Sportage R",
-                                        "96" => "Sportage R",
+                                        "96" => "Sportage R CKD",
                                         //"10" => "Sorento",
                                         //"25" => "K 2700 Cabina Simple",
                                         //"87" => "K 2700 Cabina Doble",
