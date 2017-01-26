100681907f88ab30136cec0e3bff608888358c96
Jorge Rodriguez <jorge.rodriguez@ariadna.com.ec>
2017-01-26 17:02:33 -0500

    Filtros QK

--- protected/views/layouts/rgd/filtros.php
+++ protected/views/layouts/rgd/filtros.php
@@ -54,7 +54,7 @@ $id_responsable = Yii::app()->user->getId();
                     <option value="Entrega">Entrega</option>
                     <option value="Vendido">Seguimiento-Paso 10</option>
                     <option value="Desiste">Desiste</option>
-                    <?php if($cargo_id == 70 || $cargo_id == 69 || $cargo_id == 71){ ?>
+                    <?php if($cargo_id == 70 || $cargo_id == 71){ ?>
                     <option value="qk">Campaña Quiero un Kia</option>
                     <option value="qktd">Campaña Quiero un Kia TD</option>
                     <?php } ?>
