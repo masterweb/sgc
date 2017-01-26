775aaee6e210d3c4311c894eeec0cd8d4a36487b
Jorge Rodriguez <jorge.rodriguez@ariadna.com.ec>
2017-01-26 09:55:01 -0500

    Lista de precios

--- protected/views/gestionVehiculo/_form.php
+++ protected/views/gestionVehiculo/_form.php
@@ -489,7 +489,7 @@ if (isset($id)) {
                                                                                                      
                                                     ?> 
                                                 </td>
-                                                <td><a href="<?php echo Yii::app()->request->baseUrl; ?>/images/LISTA-DE-PRECIOS-KIA-04-01-2017.pdf" target="_blank" class="btn btn-xs btn-default">Ver Precios</a></td>
+                                                <td><a href="<?php echo Yii::app()->request->baseUrl; ?>/images/LISTA-DE-PRECIOS-KIA-04-01-2017-2.pdf" target="_blank" class="btn btn-xs btn-default">Ver Precios</a></td>
                                             </tr>
                                             <?php
                                         endforeach;
