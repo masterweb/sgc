1a48a1e61cbb4a9cafbb28be91378a22be7f6691
Jorge Rodriguez <jorge.rodriguez@ariadna.com.ec>
2017-03-23 09:16:39 -0500

    Nuevas listas de precios pdf

--- protected/views/gestionVehiculo/_form.php
+++ protected/views/gestionVehiculo/_form.php
@@ -489,7 +489,7 @@ if (isset($id)) {
                                                                                                      
                                                     ?> 
                                                 </td>
-                                                <td><a href="<?php echo Yii::app()->request->baseUrl; ?>/images/LISTA-DE-PRECIOS-KIA-01-03-2017.pdf" target="_blank" class="btn btn-xs btn-default">Ver Precios</a></td>
+                                                <td><a href="<?php echo Yii::app()->request->baseUrl; ?>/images/LISTA-DE-PRECIOS-KIA-21-03-2017.pdf" target="_blank" class="btn btn-xs btn-default">Ver Precios</a></td>
                                             </tr>
                                             <?php
                                         endforeach;
